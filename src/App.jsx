26c467c9ffe585a88dbc126411c3063b3db16056
ilkhomov-web <ilhomovelyor119@gmail.com>
2025-10-04 17:50:55 +0500

    some chnage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Route, Router, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePAge";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
